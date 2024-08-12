60718c7c524c2d11b9d7d0396655a648ec764613
Matias-07 <matias.ferrari.m@gmail.com>
2024-08-11 23:26:17 -0300

    first steap register

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const rutasMain = require ('./routes/main.js');
+const Users = require('./routes/users.js')
 
 const app = express(); 
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/../public'));
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
 app.listen(4321,() => console.log('The network is online')); 
 
-app.get('/', rutasMain); 
+app.use('/', rutasMain); 
+
+app.use('/users', Users); 
